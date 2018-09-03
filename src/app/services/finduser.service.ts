fce3c9bbcb14435ac9007240452df5b69d0f4cd3
imakashrana <30438670+imakashrana@users.noreply.github.com>
2018-09-03 13:47:59 +0530

    Update finduser.service.ts

--- src/app/services/finduser.service.ts
+++ src/app/services/finduser.service.ts
@@ -7,7 +7,7 @@ import { catchError, map, tap } from 'rxjs/operators';
                                 .set('Access-Control-Allow-Origin', '*')
                                  .set('Access-Control-Allow-Methods', 'POST');
         const httpOptions =  {
-            headers:Headers
+            Headers:headers
         };
 @Injectable({
   providedIn: 'root'
